8d87a102bc06db2080e0fe6c9c985bdfd76e1be6
Amirkhan <akhatov2901@gmail.com>
2022-03-29 14:23:52 +0600

    Добавил файл и класс Elevator прописал логику в двух файлах

---
+++ Encapsulation/01_Elevator/src/Elevator.java
@@ -0,0 +1,47 @@
+public class Elevator {
+        public int  currentFloor = 0;
+        public int minFloor;
+        public int maxFloor;
+
+        public Elevator(int minFloor , int maxFloor){
+            this.minFloor = minFloor;
+            this.maxFloor = maxFloor;
+        }
+
+
+        public void moveDown(){
+            currentFloor = (currentFloor > minFloor) ? currentFloor - 1 :currentFloor ;
+        }
+
+        public void moveUp(){
+            currentFloor = (currentFloor < maxFloor) ? currentFloor + 1 :currentFloor ;
+
+        }
+
+
+        public int getCurrentFloor(){
+            return currentFloor;
+        }
+
+        public void move(int floor){
+            if(floor > minFloor && floor < maxFloor){
+                if (currentFloor < floor){
+                    while (currentFloor < floor){
+                        moveUp();
+                        System.out.println(currentFloor);
+                    }
+                } else {
+                    while (currentFloor > floor) {
+                        moveDown();
+                        System.out.println(currentFloor);
+                    }
+                }
+
+            }else{
+                System.out.println("Error out of range");
+            }
+        }
+
+
+
+}

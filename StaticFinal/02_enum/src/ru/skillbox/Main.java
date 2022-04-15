bbcb3277264bf728e64594f6e23f3e34f0638e89
Amirkhan <akhatov2901@gmail.com>
2022-04-15 15:55:11 +0600

    Добавил 2 файла, прописал логику

--- StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -1,7 +1,12 @@
 package ru.skillbox;
-
 public class Main {
-
     public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.print("Basket contains");
+        basket.add("Milk", 40,5,1);
+        System.out.println(Basket.getCount());
+        System.out.println(Basket.averagePriceOfBasket());
+        System.out.println(Basket.averagePriceInBaskets());
+
     }
 }

b4f6dbe69803e1131cec96d70273fe8a668e469f
Amirkhan <akhatov2901@gmail.com>
2022-04-24 12:53:40 +0600

    Сделал ДЗ, но незнаю как добавить буквы Ё и ё

--- PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -4,9 +4,12 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        for (int i = 1040; i <= 1103; i++){
+            char symbol = (char) i;
+            System.out.println(i + "-" + symbol);
+        }
+
+
 
     }
 }
